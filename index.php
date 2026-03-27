9764b90cb2c12b9e305ac178080ae798ed2b0c61
keano802 <9028451@student.zadkine.nl>
2026-03-27 10:49:36 +0100

    Nav toe gevoegd

--- index.php
+++ index.php
@@ -6,3 +6,24 @@ include_once 'functions.php';
 
 // Main
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Webshop games</title>
+</head>
+<nav>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="playstation.php">PlayStation</a></li>
+        <li><a href="pc.php">PC</a></li>
+        <li><a href="xbox.php">Xbox</a></li>
+        <li><a href="nintendo.php">Nintendo</a></li>
+    </ul>
+</nav>
+<body>
+    
+</body>
+</html>
