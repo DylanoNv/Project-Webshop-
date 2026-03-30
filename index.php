91becfa2fe440aa37f8f14a026e24c7ac4803dc9
Dion Breur <9027852@student.tcrmbo.nl>
2026-03-30 14:50:44 +0200

    Xbox navl, xbox css

--- index.php
+++ index.php
@@ -5,4 +5,38 @@
 include_once 'functions.php';
 
 // Main
+<<<<<<< HEAD
+// getData(tabelnaam, filter op console, waar id = 4)
+$data = getData(CATTABLE, "console", ['categorie_id' => 4]);
+print_r($data);
 ?>
+=======
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Index.css">
+    <title>Webshop games</title>
+</head>
+<nav>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="playstation.php">PlayStation</a></li>
+        <li><a href="pc.php">PC</a></li>
+        <li><a href="xbox.php">Xbox</a></li>
+        <li><a href="nintendo.php">Nintendo</a></li>
+    </ul>
+</nav>
+<body>
+
+
+
+
+
+<footer>Contact Information</footer>
+</body>
+</html>
+>>>>>>> f8b9ef66664198b15e761157abc9d8f04f6a1e0e
---
+++ playstation.php
@@ -0,0 +1,36 @@
+<?php
+<<<<<<< HEAD
+// Auteur: 
+// Functie: Tonen van Playstation producten
+
+// Initialisatie
+include_once 'functions.php';
+
+// Main
+
+?>
+=======
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link rel="stylesheet" href="Index.css">
+    <title>Webshop games</title>
+</head>
+<nav>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="playstation.php">PlayStation</a></li>
+        <li><a href="pc.php">PC</a></li>
+        <li><a href="xbox.php">Xbox</a></li>
+        <li><a href="nintendo.php">Nintendo</a></li>
+    </ul>
+</nav>
+<body>
+    
+</body>
+</html>
+>>>>>>> f8b9ef66664198b15e761157abc9d8f04f6a1e0e
