73a67077a55a6f15262a2e3f437380d20f165d4c
Dion Breur <9027852@student.tcrmbo.nl>
2026-03-30 14:53:21 +0200

    Update xbox.php

---
+++ xbox.php
@@ -0,0 +1,43 @@
+<?php
+// Auteur: Dion Breur
+// Functie: Tonen van Xbox producten
+
+// Initialisatie
+include_once 'functions.php';
+
+// Main
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Xbox Producten</title>
+    <link rel="stylesheet" href="scss/main.css">
+</head>
+<body>
+    <header>
+        <img class="logo" src="img/Logo.png">
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="playstation.php">Playstation</a></li>
+                <li><a href="pc.php">PC</a></li>
+                <li><a href="xbox.php">Xbox</a></li>
+                <li><a href="nintendo.php">Nintendo</a></li>
+            </ul>
+        </nav>
+        <img class="winkelmandje-img" src="img/Winkelmandje.png">
+    </header>
+
+    <main>
+
+    </main>
+
+    <footer>
+
+    </footer>
+</body>
+</html>
