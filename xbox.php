bfd996d12edb90d229625467ebec1b3d1e0be431
Dion Breur <9027852@student.tcrmbo.nl>
2026-04-02 13:52:58 +0200

    Add platform pictograms and Xbox favicon
    Add five platform pictogram images (homepage-picto.png, nintendo-picto.png, pc-picto.png, ps-picto.png, xbox-picto.png) to img/. Update xbox.php to include a link to img/xbox-picto.png as the page favicon so the Xbox page displays the new pictogram.

--- xbox.php
+++ xbox.php
@@ -31,6 +31,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Xbox Producten</title>
+    <link rel="shortcut icon" href="img/xbox-picto.png" type="image/x-icon">
     <link rel="stylesheet" href="scss/main.css">
 </head>
 <body class="xbox-page">
