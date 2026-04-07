c353c01f2d7fe356f6c6be12a7e89f5ffa33aecd
SvenTcr <9029665@student.tcrmbo.nl>
2026-04-07 16:09:27 +0200

    testje

---
+++ test.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PC Producten</title>
+    <link rel="shortcut icon" href="img/pc-picto.png" type="image/x-icon">
+    <link rel="stylesheet" href="scss/main.css">
+</head>
+<body class="pc-page">
+    <header>
+        <img class="logo" src="img/Logo.png">
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="playstation.php">Playstation</a></li>
+                <li><a class="selected-page" href="#">PC</a></li>
+                <li><a href="xbox.php">Xbox</a></li>
+                <li><a href="nintendo.php">Nintendo</a></li>
+            </ul>
+        </nav>
+        <div id="headerUserMenu" class="header-user-menu">
+            <?php if (isLoggedIn()): ?>
+                <!-- <span class="user-info">Welkom, <strong><?php echo htmlspecialchars($_SESSION['username']); ?></strong></span> -->
+                <a href="logout.php" class="btn-logout">Uitloggen</a>
+            <?php else: ?>
+                <a href="login.php">Inloggen</a>
+                <a href="register.php">Registreren</a>
+            <?php endif; ?>
+        </div>
+        <a href="winkelmandje.php"><img class="winkelmandje-img" src="img/Winkelmandje.png"></a>
+    </header>
+
+    <main>
+            <form class="search-bar" action="" method="post">
+                <input type="text" name="search-inp" placeholder="Typ hier je zoekterm..." value="<?php echo isset($_POST['search-inp']) ? htmlspecialchars($_POST['search-inp']) : ''; ?>">
+                <section class="filter-wrapper">
+                    <section class="filter-icon">
+                        <span class="bar long"></span>
+                        <span class="bar medium"></span>
+                        <span class="bar short"></span>
+                    </section>
+
+                    <section class="filter-content">
+                        <h4>Sorteren op:</h4>
+                        <label for="sortPrice"><input type="radio" name="sortPrice" value="price_low"> Prijs: Laag - Hoog</label>
+                        <label for="sortPrice"><input type="radio" name="sortPrice" value="price_high"> Prijs: Hoog - Laag</label>
+
+                        <button type="submit">Toepassen</button>
+                    </section>
+                </section>
+                <button type="submit" name="zoeken">Zoeken</button>
+            </form>
+
+        <?php
+        showProducts($products);
+        ?>
+
+        <section id="gameModal" class="game-modal">
+            <section class="modal-content">
+                <span onclick="closeModal()">&times;</span>
+                <img id=modalImg src="" alt="Game afbeelding">
+                <h2 id="modalTitle"></h2>
+                <p id="modalPrice"></p>
+                <form class="add-to-wish-and-cart" action="" method="post">
+                    <input type="hidden" name="product_id" id="modalProductId">
+                    <button name="addToCart" type="submit" class="winkelmand-btn">
+                        Voeg toe aan winkelmandje
+                        <img src="img/Add-to-cart.png" alt="Winkelmandje">
+                    </button>
+                    <button name="addToWish" type="submit" class="wishlist-btn">
+                        Voeg toe aan wishlist
+                        <img src="img/Add_to_wishlist.png" alt="Wishlist">
+                    </button>
+                </form>
+            </section>
+        </section>
+    </main>
+
+    <footer>
+
+    </footer>
+
+    <script>
+        function openModal(id, name, price, imgSrc){
+            const modalImageElement = document.getElementById('modalImg');
+            modalImageElement.src = imgSrc;
+            modalImageElement.alt = "Afbeelding van " + name;
+
+            document.getElementById('modalTitle').innerText = name;
+            document.getElementById('modalPrice').innerText = "Prijs: €" + price;
+            document.getElementById('modalProductId').value = id;
+            document.getElementById('gameModal').style.display = 'block';
+            document.getElementById('headerUserMenu').style.display = 'none';
+        }
+        function closeModal(){
+            document.getElementById('gameModal').style.display = 'none';
+            document.getElementById('headerUserMeny').style.display = 'block';
+            document.getElementById('modalImg').src = "";
+        }
+    </script>
+</body>
+</html>
